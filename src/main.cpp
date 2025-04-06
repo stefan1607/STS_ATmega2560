9c34a34a4d1f15cff66cab32fbbc30b884a1f4b3
stefan1607 <65455310+stefan1607@users.noreply.github.com>
2025-04-06 12:41:36 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+//#include <avr/io.h>
+//#include <avr/interrupt.h>
+#include "avr8-stub.h"
+
+
+// put function declarations here:
+uint8_t count =0;
+
+void setup()
+{
+
+  //debug_init();
+
+  // put your setup code here, to run once:
+  Serial.begin(1152000);
+  delay(2000);
+  Serial.println("Hello, world!");
+  pinMode(LED_BUILTIN, OUTPUT);  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print(".");
+  count++;
+  if (count == 20)
+  {
+    count = 0;
+    Serial.println();
+  }
+
+  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+  delay(1000); // wait for a second
+  digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
+  delay(1000); // wait for a second
+
+}
